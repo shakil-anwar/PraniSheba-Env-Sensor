a5c82a3e1bcd9d8ac8293adec510bec0dd6f7bb8
shuvangkar <sshuvo93@gmail.com>
2020-10-30 18:40:37 +0600

    dead code deleted

--- Cowshed/Cowshed.ino
+++ Cowshed/Cowshed.ino
@@ -6,20 +6,5 @@ void setup()
 }
 void loop()
 {
-//  Serial.print(F("Second : "));Serial.println(second());
-
-  //  Serial.print(F("MQ4 | Rs: ")); Serial.print(mq4.Rs);
-  //  Serial.print(F(" | PPM: ")); Serial.println(mq4.getPPM());
-  //Send message to receiver
-  //  const char text[] = "Hello World";
-  //  radio.write(&text, sizeof(text));
-  //
-  //  Serial.print(F("Humidity: "));Serial.println(getSenorReading() -> hum);
-  //  set_led_level(3, 3);
-  //  delay(1000);
-  //  led_blink();
-
-
-
   scheduler.run();
 }
