c448ee0f01a9f97cfef5d5fb9358a4ad61676964
shakil-anwar <shakil7531@gmail.com>
2021-06-06 13:34:29 +0600

    final production file for v0.6.0

---
+++ Cowshed/src/lib/realTime/src/realTime.h
@@ -0,0 +1,36 @@
+#ifndef _SYNC_TIME_H
+#define _SYNC_TIME_H
+#include <Arduino.h>
+// #include <stdint.h>
+// #include <stddef.h>
+// #include <stdbool.h>
+
+typedef uint32_t (*timeGetter_t)(void);
+typedef void (*timeSetter_t)(uint32_t);
+
+typedef enum tState_t
+{
+  WAIT,
+  MINUTELY,
+  HOURLY,
+  DAILY,
+};
+typedef enum RT_SYNC_STATUS_t
+{
+  NTP_SYNCED,
+  RTC_SYNCED,
+  UNSYNCED,
+};
+
+
+void rtAttachRTC(timeGetter_t getter, timeSetter_t setter);
+void rtAttachFastRTC(timeGetter_t getter, timeSetter_t setter,void (*StartRtc)(void));
+void rtBegin(timeGetter_t getntp = NULL);
+RT_SYNC_STATUS_t rtSync(uint32_t uTime);
+RT_SYNC_STATUS_t rtsync();
+// uint32_t second();
+// uint32_t ms();
+tState_t rtLoop();
+
+
+#endif 
